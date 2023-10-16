334a0245a3dfd4ca2656ead6184f41436145c0e8
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2023-10-16 11:48:36 +0200

    new h1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <h1>hello from kristian</h1>
+        <h1>hello from World</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
